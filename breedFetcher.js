7a972e12cc214ca0b8f2d85ddbed5b2392dc947e
vagrant <avedj94@hotmail.com>
2020-11-05 19:24:25 +0000

    test extracted data with mocha and chai

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,15 +1,37 @@
 const request = require('request');
-const input = process.argv.slice(2);
 
-request("https://api.thecatapi.com/v1/breeds/search?q=" + input, (error, response, body) => {
-  if (error) {
-    console.log("Request failed!");
+const fetchBreedDescription = function(breedName, callback) {
+  request("https://api.thecatapi.com/v1/breeds/search?q=" + breedName, (error, response, body) => {
+  //console.log(response.statusCode);
+  const data = JSON.parse(body);
+
+  // if (error) {
+  //     console.log('THERE IS AN ERROR');
+  //     callback(error, null);
+  //   } else {
+
+  //     if (!data[0]) {
+  //       callback(error, null);
+  //     } else {
+  //       callback(null, data[0].description);
+  //     }
+  //   }
+
+  if (response.statusCode !== 200) {
+    let requestError = "Api call failed with status code " + response.statusCode;
+    callback(requestError, null);
+  } else if (data.length === 0) {
+    let requestError = "Breed doesn't exist";
+    callback(requestError, null);
   } else {
-    const data = JSON.parse(body);
-    if (!data[0]) {
-      console.log("Breed not found!");
-    } else {
-      console.log(data[0].description);
-    }
+    callback(null, data[0].description.trim());
   }
-});
+
+
+
+
+
+  });
+};
+
+module.exports = { fetchBreedDescription };
